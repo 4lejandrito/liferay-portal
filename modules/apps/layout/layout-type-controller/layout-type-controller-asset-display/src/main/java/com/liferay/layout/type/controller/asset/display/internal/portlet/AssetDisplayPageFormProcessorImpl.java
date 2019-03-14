c14a4d5ce98778431ec433c1b99cbf99c030f668
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-03-14 15:09:53 +0100

    LPS-90760 Replacing use in journal too

--- modules/apps/layout/layout-type-controller/layout-type-controller-asset-display/src/main/java/com/liferay/layout/type/controller/asset/display/internal/portlet/AssetDisplayPageFormProcessorImpl.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-asset-display/src/main/java/com/liferay/layout/type/controller/asset/display/internal/portlet/AssetDisplayPageFormProcessorImpl.java
@@ -14,9 +14,12 @@
 
 package com.liferay.layout.type.controller.asset.display.internal.portlet;
 
+import com.liferay.asset.display.page.constants.AssetDisplayPageConstants;
 import com.liferay.asset.display.page.model.AssetDisplayPageEntry;
 import com.liferay.asset.display.page.portlet.AssetDisplayPageEntryFormProcessor;
 import com.liferay.asset.display.page.service.AssetDisplayPageEntryLocalService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
@@ -24,6 +27,8 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Date;
 
 import javax.portlet.PortletRequest;
 
@@ -55,7 +60,13 @@ public class AssetDisplayPageFormProcessorImpl
 
 		int displayPageType = ParamUtil.getInteger(request, "displayPageType");
 
-		if (assetDisplayPageEntry == null) {
+		if (displayPageType == AssetDisplayPageConstants.TYPE_NONE) {
+			if (assetDisplayPageEntry != null) {
+				_assetDisplayPageEntryLocalService.deleteAssetDisplayPageEntry(
+					themeDisplay.getScopeGroupId(), classNameId, classPK);
+			}
+		}
+		else if (assetDisplayPageEntry == null) {
 			ServiceContext serviceContext = ServiceContextFactory.getInstance(
 				className, request);
 
@@ -65,18 +76,30 @@ public class AssetDisplayPageFormProcessorImpl
 				serviceContext);
 		}
 		else {
-			assetDisplayPageEntry.setLayoutPageTemplateEntryId(
-				assetDisplayPageId);
-			assetDisplayPageEntry.setType(displayPageType);
-
 			_assetDisplayPageEntryLocalService.updateAssetDisplayPageEntry(
-				assetDisplayPageEntry);
+				assetDisplayPageEntry.getAssetDisplayPageEntryId(),
+				assetDisplayPageId, displayPageType);
+		}
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.fetchLayoutPageTemplateEntry(
+				assetDisplayPageId);
+
+		if (layoutPageTemplateEntry != null) {
+			layoutPageTemplateEntry.setModifiedDate(new Date());
+
+			_layoutPageTemplateEntryLocalService.updateLayoutPageTemplateEntry(
+				layoutPageTemplateEntry);
 		}
 	}
 
 	@Reference
 	private AssetDisplayPageEntryLocalService
 		_assetDisplayPageEntryLocalService;
+
+	@Reference
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
 
 	@Reference
 	private Portal _portal;
