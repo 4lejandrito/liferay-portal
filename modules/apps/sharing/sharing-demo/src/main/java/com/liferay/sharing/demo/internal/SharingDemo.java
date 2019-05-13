593aab5da095d317a22b156246f1aa52010ccbb8
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-05-13 13:43:04 +0200

    Initializing permission checker in sharing-demo

--- modules/apps/sharing/sharing-demo/src/main/java/com/liferay/sharing/demo/internal/SharingDemo.java
+++ modules/apps/sharing/sharing-demo/src/main/java/com/liferay/sharing/demo/internal/SharingDemo.java
@@ -26,6 +26,8 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -69,6 +71,9 @@ public class SharingDemo extends BasePortalInstanceLifecycleListener {
 		serviceContext.setAddGuestPermissions(true);
 		serviceContext.setScopeGroupId(group.getGroupId());
 		serviceContext.setUserId(sharerUser.getUserId());
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(sharerUser));
 
 		for (int i = 0; i < 15; i++) {
 			User user = _basicUserDemoDataCreator.create(
