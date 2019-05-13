af00baeecd09eefa41a6fae86e55d2765d12ee4b
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-05-13 13:42:46 +0200

    No longer needed

--- modules/apps/sharing/sharing-document-library-test/src/testIntegration/java/com/liferay/sharing/document/library/internal/security/permission/resource/test/SharingEntryDLFileEntryModelResourcePermissionLogicTest.java
+++ modules/apps/sharing/sharing-document-library-test/src/testIntegration/java/com/liferay/sharing/document/library/internal/security/permission/resource/test/SharingEntryDLFileEntryModelResourcePermissionLogicTest.java
@@ -64,7 +64,7 @@ import org.junit.runner.RunWith;
  * @author Sergio González
  */
 @RunWith(Arquillian.class)
-public class SharingEntryDLFileEntryModelResourcePermissionRegistrarTest {
+public class SharingEntryDLFileEntryModelResourcePermissionLogicTest {
 
 	@ClassRule
 	@Rule
