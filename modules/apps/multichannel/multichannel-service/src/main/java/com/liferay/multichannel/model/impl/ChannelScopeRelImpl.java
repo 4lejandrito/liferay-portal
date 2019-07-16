5852832f14fa484fd299989d68c12c3b3dfcd7f7
Adolfo Pérez <adolfo.perez@liferay.com>
2019-07-16 14:35:06 +0200

    LPS-00000 Autogenerated

--- modules/apps/multichannel/multichannel-service/src/main/java/com/liferay/multichannel/model/impl/ChannelScopeRelImpl.java
+++ modules/apps/multichannel/multichannel-service/src/main/java/com/liferay/multichannel/model/impl/ChannelScopeRelImpl.java
@@ -28,7 +28,7 @@ import org.osgi.annotation.versioning.ProviderType;
 @ProviderType
 public class ChannelScopeRelImpl extends ChannelScopeRelBaseImpl {
 
-	/*
+	/**
 	 * NOTE FOR DEVELOPERS:
 	 *
 	 * Never reference this class directly. All methods that expect a channel scope rel model instance should use the {@link com.liferay.multichannel.model.ChannelScopeRel} interface instead.
