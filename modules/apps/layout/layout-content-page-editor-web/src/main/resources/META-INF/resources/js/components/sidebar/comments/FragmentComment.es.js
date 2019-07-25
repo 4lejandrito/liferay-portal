0d8e6aceae26df483c54bfebcecffce71d8fe214
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-07-25 17:22:31 +0200

    LPS-98738 Adding hyphenation to the comment's content

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/sidebar/comments/FragmentComment.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/sidebar/comments/FragmentComment.es.js
@@ -89,7 +89,7 @@ const FragmentComment = props => {
 				/>
 			) : (
 				<p
-					className="text-secondary"
+					className="content text-secondary"
 					dangerouslySetInnerHTML={{__html: props.body}}
 				/>
 			)}
