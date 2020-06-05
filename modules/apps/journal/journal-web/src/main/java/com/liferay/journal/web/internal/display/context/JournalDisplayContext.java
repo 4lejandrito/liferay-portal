bde22506da5950c4a7b7991fd6811ae366f6c108
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2020-06-05 16:08:38 +0200

    LPS-114865 Export option does not need a specific permission

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -354,8 +354,14 @@ public class JournalDisplayContext {
 				_themeDisplay.getPermissionChecker(), article,
 				ActionKeys.UPDATE)) {
 
-			availableActions.add("exportTranslation");
 			availableActions.add("moveEntries");
+		}
+
+		if (JournalArticlePermission.contains(
+			_themeDisplay.getPermissionChecker(), article,
+			ActionKeys.VIEW)) {
+
+			availableActions.add("exportTranslation");
 		}
 
 		return com.liferay.petra.string.StringUtil.merge(
