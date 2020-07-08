dbb4e78da3ad0f92abc74cbe76a97f3df0a87c37
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-07-08 17:27:09 +0200

    LPS-116788 Applying builder pattern with mandatory type

--- modules/apps/info/info-api/src/main/java/com/liferay/info/field/InfoField.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/field/InfoField.java
@@ -28,23 +28,47 @@ import java.util.Objects;
  */
 public class InfoField implements InfoFieldSetEntry {
 
+	public static InfoField.Builder builder(InfoFieldType infoFieldType) {
+		return new InfoField.Builder(infoFieldType);
+	}
+
+	/**
+	 * @deprecated As of Athanasius (7.3.x)
+	 */
+	@Deprecated
 	public InfoField(
 		InfoFieldType infoFieldType,
 		InfoLocalizedValue<String> labelInfoLocalizedValue, boolean localizable,
 		String name) {
 
-		this(infoFieldType, labelInfoLocalizedValue, name);
-
-		_localizable = localizable;
+		this(
+			builder(
+				infoFieldType
+			).labelInfoLocalizedValue(
+				labelInfoLocalizedValue
+			).name(
+				name
+			).localizable(
+				localizable
+			));
 	}
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x)
+	 */
+	@Deprecated
 	public InfoField(
 		InfoFieldType infoFieldType,
 		InfoLocalizedValue<String> labelInfoLocalizedValue, String name) {
 
-		_infoFieldType = infoFieldType;
-		_labelInfoLocalizedValue = labelInfoLocalizedValue;
-		_name = name;
+		this(
+			builder(
+				infoFieldType
+			).labelInfoLocalizedValue(
+				labelInfoLocalizedValue
+			).name(
+				name
+			));
 	}
 
 	@Override
@@ -59,11 +83,13 @@ public class InfoField implements InfoFieldSetEntry {
 
 		InfoField infoDisplayField = (InfoField)object;
 
-		if (Objects.equals(_infoFieldType, infoDisplayField._infoFieldType) &&
+		if (Objects.equals(
+				_builder._infoFieldType,
+				infoDisplayField._builder._infoFieldType) &&
 			Objects.equals(
-				_labelInfoLocalizedValue,
-				infoDisplayField._labelInfoLocalizedValue) &&
-			Objects.equals(_name, infoDisplayField._name)) {
+				_builder._labelInfoLocalizedValue,
+				infoDisplayField._builder._labelInfoLocalizedValue) &&
+			Objects.equals(_builder._name, infoDisplayField._builder._name)) {
 
 			return true;
 		}
@@ -72,35 +98,35 @@ public class InfoField implements InfoFieldSetEntry {
 	}
 
 	public InfoFieldType getInfoFieldType() {
-		return _infoFieldType;
+		return _builder._infoFieldType;
 	}
 
 	@Override
 	public String getLabel(Locale locale) {
-		return _labelInfoLocalizedValue.getValue(locale);
+		return _builder._labelInfoLocalizedValue.getValue(locale);
 	}
 
 	@Override
 	public InfoLocalizedValue<String> getLabelInfoLocalizedValue() {
-		return _labelInfoLocalizedValue;
+		return _builder._labelInfoLocalizedValue;
 	}
 
 	@Override
 	public String getName() {
-		return _name;
+		return _builder._name;
 	}
 
 	@Override
 	public int hashCode() {
-		int hash = HashUtil.hash(0, _infoFieldType);
+		int hash = HashUtil.hash(0, _builder._infoFieldType);
 
-		hash = HashUtil.hash(hash, _labelInfoLocalizedValue);
+		hash = HashUtil.hash(hash, _builder._labelInfoLocalizedValue);
 
-		return HashUtil.hash(hash, _name);
+		return HashUtil.hash(hash, _builder._name);
 	}
 
 	public boolean isLocalizable() {
-		return _localizable;
+		return _builder._localizable;
 	}
 
 	@Override
@@ -108,17 +134,55 @@ public class InfoField implements InfoFieldSetEntry {
 		StringBundler sb = new StringBundler(5);
 
 		sb.append("{name: ");
-		sb.append(_name);
+		sb.append(_builder._name);
 		sb.append(", type: ");
-		sb.append(_infoFieldType.getName());
+		sb.append(_builder._infoFieldType.getName());
 		sb.append("}");
 
 		return sb.toString();
 	}
 
-	private final InfoFieldType _infoFieldType;
-	private final InfoLocalizedValue<String> _labelInfoLocalizedValue;
-	private boolean _localizable;
-	private final String _name;
+	public static class Builder {
+
+		public InfoField build() {
+			return new InfoField(this);
+		}
+
+		public InfoField.Builder labelInfoLocalizedValue(
+			InfoLocalizedValue<String> labelInfoLocalizedValue) {
+
+			_labelInfoLocalizedValue = labelInfoLocalizedValue;
+
+			return this;
+		}
+
+		public InfoField.Builder localizable(boolean localizable) {
+			_localizable = localizable;
+
+			return this;
+		}
+
+		public InfoField.Builder name(String name) {
+			_name = name;
+
+			return this;
+		}
+
+		private Builder(InfoFieldType infoFieldType) {
+			_infoFieldType = infoFieldType;
+		}
+
+		private final InfoFieldType _infoFieldType;
+		private InfoLocalizedValue<String> _labelInfoLocalizedValue;
+		private boolean _localizable;
+		private String _name;
+
+	}
+
+	private InfoField(InfoField.Builder builder) {
+		_builder = builder;
+	}
+
+	private Builder _builder;
 
 }
