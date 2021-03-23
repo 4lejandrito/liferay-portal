6bf92c0200e6b52f41d55c9bcea2f15fa78c1eda
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-03-23 14:34:02 +0100

    LPS-127788 Using the right type and manually setting the default value (0) to avoid potential issues with diffferent DBs

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeDLFileEntryType.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeDLFileEntryType.java
@@ -27,8 +27,10 @@ public class UpgradeDLFileEntryType extends UpgradeProcess {
 		if (!hasColumn("DLFileEntryType", "scope")) {
 			alter(
 				DLFileEntryTypeTable.class,
-				new AlterTableAddColumn("scope", "INT 0"));
+				new AlterTableAddColumn("scope", "INTEGER"));
 		}
+
+		runSQL("update DLFileEntryType set scope = 0");
 	}
 
 }
