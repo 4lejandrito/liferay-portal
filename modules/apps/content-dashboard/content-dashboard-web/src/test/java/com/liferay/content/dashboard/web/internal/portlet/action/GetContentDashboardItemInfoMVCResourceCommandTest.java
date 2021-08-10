469c2448831f6c03ed8547f38f6a3fec5bcba89e
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-08-10 15:50:18 +0200

    LPS-131186 SF

--- modules/apps/content-dashboard/content-dashboard-web/src/test/java/com/liferay/content/dashboard/web/internal/portlet/action/GetContentDashboardItemInfoMVCResourceCommandTest.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/test/java/com/liferay/content/dashboard/web/internal/portlet/action/GetContentDashboardItemInfoMVCResourceCommandTest.java
@@ -291,7 +291,7 @@ public class GetContentDashboardItemInfoMVCResourceCommandTest {
 			@Override
 			public JSONObject getSpecificInformationJSONObject(
 				Locale locale, LiferayPortletResponse liferayPortletResponse,
-				String back) {
+				String backURL) {
 
 				return null;
 			}
