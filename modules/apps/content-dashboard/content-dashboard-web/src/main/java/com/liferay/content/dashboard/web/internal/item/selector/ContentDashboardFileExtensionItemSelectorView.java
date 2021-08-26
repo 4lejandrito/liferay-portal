4lejandrito/liferay-portal

commit a01a5696075ec9c2dfc054731593850757b72aba

LPS-133354 Showing only existing extensions

modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/selector/ContentDashboardFileExtensionItemSelectorView.java
@@ -16,6 +16,7 @@ package com.liferay.content.dashboard.web.internal.item.selector;
 
 import com.liferay.content.dashboard.web.internal.display.context.ContentDashboardFileExtensionItemSelectorViewDisplayContext;
 import com.liferay.content.dashboard.web.internal.item.selector.criteria.content.dashboard.file.extension.criterion.ContentDashboardFileExtensionItemSelectorCriterion;
+import com.liferay.content.dashboard.web.internal.searcher.ContentDashboardSearchRequestBuilderFactory;
 import com.liferay.document.library.configuration.DLConfiguration;
 import com.liferay.item.selector.ItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorView;
@@ -23,26 +24,37 @@ import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.search.SearchContextFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.search.aggregation.Aggregations;
+import com.liferay.portal.search.aggregation.bucket.Bucket;
+import com.liferay.portal.search.aggregation.bucket.TermsAggregationResult;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
 import com.liferay.portal.util.PropsValues;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.portlet.PortletURL;
@@ -52,6 +64,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -117,42 +130,47 @@ public class ContentDashboardFileExtensionItemSelectorView
 	protected void activate(Map<String, Object> properties) {
 		_dlConfiguration = ConfigurableUtil.createConfigurable(
 			DLConfiguration.class, properties);
-	}
 
-	private JSONObject _getContentDashboardFileExtensionGroupJSONObject(
-		Set<String> checkedFileExtensions, String icon, String labelKey,
-		Locale locale, String[] mimeTypes) {
+		_fileExtensionGroups = Arrays.asList(
+			new FileExtensionGroup(
+				"document-multimedia", "audio",
+				PropsValues.DL_FILE_ENTRY_PREVIEW_AUDIO_MIME_TYPES),
+			new FileExtensionGroup(
+				"document-code", "code", _dlConfiguration.codeFileMimeTypes()),
+			new FileExtensionGroup(
+				"document-compressed", "compressed",
+				_dlConfiguration.compressedFileMimeTypes()),
+			new FileExtensionGroup(
+				"document-image", "image",
+				PropsValues.DL_FILE_ENTRY_PREVIEW_IMAGE_MIME_TYPES),
+			new FileExtensionGroup(
+				"document-presentation", "presentation",
+				_dlConfiguration.presentationFileMimeTypes()),
+			new FileExtensionGroup(
+				"document-table", "spreadsheet",
+				_dlConfiguration.spreadSheetFileMimeTypes()),
+			new FileExtensionGroup(
+				"document-text", "text", _dlConfiguration.textFileMimeTypes()),
+			new FileExtensionGroup(
+				"document-pdf", "vectorial",
+				_dlConfiguration.vectorialFileMimeTypes()),
+			new FileExtensionGroup(
+				"document-multimedia", "video",
+				PropsValues.DL_FILE_ENTRY_PREVIEW_VIDEO_MIME_TYPES));
 
-		JSONArray fileExtensionsJSONArray = JSONFactoryUtil.createJSONArray();
+		_fileExtensionFileExtensionGroupKeys = new HashMap<>();
 
-		Stream<String> stream = Arrays.stream(mimeTypes);
+		for (FileExtensionGroup fileExtensionGroup : _fileExtensionGroups) {
+			for (String mimeType : fileExtensionGroup.getMimeTypes()) {
+				for (String fileExtension :
+						MimeTypesUtil.getExtensions(mimeType)) {
 
-		stream.map(
-			MimeTypesUtil::getExtensions
-		).flatMap(
-			Set::stream
-		).sorted(
-		).distinct(
-		).map(
-			fileExtension -> fileExtension.replaceAll("^\\.", StringPool.BLANK)
-		).forEach(
-			fileExtension -> fileExtensionsJSONArray.put(
-				JSONUtil.put(
-					"fileExtension", fileExtension
-				).put(
-					"selected", checkedFileExtensions.contains(fileExtension)
-				))
-		);
-
-		return JSONUtil.put(
-			"fileExtensions", fileExtensionsJSONArray
-		).put(
-			"icon", icon
-		).put(
-			"label",
-			LanguageUtil.get(
-				ResourceBundleUtil.getBundle(locale, getClass()), labelKey)
-		);
+					_fileExtensionFileExtensionGroupKeys.put(
+						fileExtension.replaceAll("^\\.", StringPool.BLANK),
+						fileExtensionGroup.getKey());
+				}
+			}
+		}
 	}
 
 	private JSONArray _getContentDashboardFileExtensionGroupsJSONArray(
@@ -164,47 +182,143 @@ public class ContentDashboardFileExtensionItemSelectorView
 		ThemeDisplay themeDisplay = (ThemeDisplay)servletRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		Locale locale = themeDisplay.getLocale();
+		List<String> existingFileExtensions = _getExistingFileExtensions(
+			themeDisplay.getRequest());
+
+		Stream<String> stream = existingFileExtensions.stream();
+
+		Map<String, List<String>> fileExtensionGroupKeyFileExtensions =
+			stream.filter(
+				Objects::nonNull
+			).collect(
+				Collectors.groupingBy(
+					fileExtension ->
+						_fileExtensionFileExtensionGroupKeys.getOrDefault(
+							fileExtension, "none"))
+			);
+
+		Stream<FileExtensionGroup> fileExtensionGroupStream =
+			_fileExtensionGroups.stream();
 
 		return JSONUtil.putAll(
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-multimedia", "audio", locale,
-				PropsValues.DL_FILE_ENTRY_PREVIEW_AUDIO_MIME_TYPES),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-code", "code", locale,
-				_dlConfiguration.codeFileMimeTypes()),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-compressed", "compressed",
-				locale, _dlConfiguration.compressedFileMimeTypes()),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-image", "image", locale,
-				PropsValues.DL_FILE_ENTRY_PREVIEW_IMAGE_MIME_TYPES),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-presentation", "presentation",
-				locale, _dlConfiguration.presentationFileMimeTypes()),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-table", "spreadsheet", locale,
-				_dlConfiguration.spreadSheetFileMimeTypes()),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-text", "text", locale,
-				_dlConfiguration.textFileMimeTypes()),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-pdf", "vectorial", locale,
-				_dlConfiguration.vectorialFileMimeTypes()),
-			_getContentDashboardFileExtensionGroupJSONObject(
-				checkedFileExtensions, "document-multimedia", "video", locale,
-				PropsValues.DL_FILE_ENTRY_PREVIEW_VIDEO_MIME_TYPES));
+			fileExtensionGroupStream.map(
+				fileExtensionGroup -> {
+					List<String> fileExtensions =
+						fileExtensionGroupKeyFileExtensions.get(
+							fileExtensionGroup.getKey());
+
+					if (ListUtil.isEmpty(fileExtensions)) {
+						return null;
+					}
+
+					Stream<String> fileExtensionsStream =
+						fileExtensions.stream();
+
+					return JSONUtil.put(
+						"fileExtensions",
+						JSONUtil.putAll(
+							fileExtensionsStream.map(
+								fileExtension -> JSONUtil.put(
+									"fileExtension", fileExtension
+								).put(
+									"selected",
+									checkedFileExtensions.contains(
+										fileExtension)
+								)
+							).toArray())
+					).put(
+						"icon", fileExtensionGroup.getIcon()
+					).put(
+						"label",
+						LanguageUtil.get(
+							ResourceBundleUtil.getBundle(
+								themeDisplay.getLocale(), getClass()),
+							fileExtensionGroup.getKey())
+					);
+				}
+			).filter(
+				Objects::nonNull
+			).toArray());
+	}
+
+	private List<String> _getExistingFileExtensions(
+		HttpServletRequest httpServletRequest) {
+
+		SearchRequestBuilder searchRequestBuilder =
+			_contentDashboardSearchRequestBuilderFactory.builder(
+				SearchContextFactory.getInstance(httpServletRequest));
+
+		SearchResponse searchResponse = _searcher.search(
+			searchRequestBuilder.addAggregation(
+				_aggregations.terms("extensions", "extension")
+			).size(
+				0
+			).build());
+
+		TermsAggregationResult termsAggregationResult =
+			(TermsAggregationResult)searchResponse.getAggregationResult(
+				"extensions");
+
+		Collection<Bucket> buckets = termsAggregationResult.getBuckets();
+
+		Stream<Bucket> stream = buckets.stream();
+
+		return stream.map(
+			bucket -> bucket.getKey()
+		).collect(
+			Collectors.toList()
+		);
 	}
 
 	private static final List<ItemSelectorReturnType>
 		_supportedItemSelectorReturnTypes = Collections.singletonList(
 			new UUIDItemSelectorReturnType());
 
+	@Reference
+	private Aggregations _aggregations;
+
+	@Reference
+	private ContentDashboardSearchRequestBuilderFactory
+		_contentDashboardSearchRequestBuilderFactory;
+
 	private volatile DLConfiguration _dlConfiguration;
+	private volatile Map<String, String> _fileExtensionFileExtensionGroupKeys =
+		new HashMap<>();
+	private volatile List<FileExtensionGroup> _fileExtensionGroups =
+		new ArrayList<>();
+
+	@Reference
+	private Searcher _searcher;
 
 	@Reference(
 		target = "(osgi.web.symbolicname=com.liferay.content.dashboard.web)"
 	)
 	private ServletContext _servletContext;
+
+	private class FileExtensionGroup {
+
+		public FileExtensionGroup(String icon, String key, String[] mimeTypes) {
+			_icon = icon;
+			_key = key;
+			_mimeTypes = mimeTypes;
+		}
+
+		public String getIcon() {
+			return _icon;
+		}
+
+		public String getKey() {
+			return _key;
+		}
+
+		public List<String> getMimeTypes() {
+			return Arrays.asList(_mimeTypes);
+		}
+
+		private final String _icon;
+		private final String _key;
+		private final String[] _mimeTypes;
+
+	}
 
 }

modules/apps/content-dashboard/content-dashboard-test/src/testIntegration/java/com/liferay/content/dashboard/web/internal/item/selector/test/ContentDashboardFileExtensionItemSelectorViewTest.java
@@ -15,11 +15,15 @@
 package com.liferay.content.dashboard.web.internal.item.selector.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.content.dashboard.web.test.util.ContentDashboardTestUtil;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalServiceUtil;
 import com.liferay.item.selector.ItemSelectorCriterion;
 import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderRequest;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderResponse;
@@ -30,12 +34,15 @@ import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.JavaConstants;
-import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.util.Map;
 
@@ -61,6 +68,7 @@ public class ContentDashboardFileExtensionItemSelectorViewTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE,
 			SynchronousDestinationTestRule.INSTANCE);
 
 	@Before
@@ -70,6 +78,16 @@ public class ContentDashboardFileExtensionItemSelectorViewTest {
 
 	@Test
 	public void testGetData() throws Exception {
+		_addFileEntry("java");
+		_addFileEntry("jpg");
+		_addFileEntry("mp3");
+		_addFileEntry("mp4");
+		_addFileEntry("pdf");
+		_addFileEntry("ppt");
+		_addFileEntry("txt");
+		_addFileEntry("xls");
+		_addFileEntry("zip");
+
 		Map<String, Object> data = _getData();
 
 		JSONArray fileExtensionGroupsJSONArray = (JSONArray)data.get(
@@ -78,38 +96,60 @@ public class ContentDashboardFileExtensionItemSelectorViewTest {
 		Assert.assertEquals(9, fileExtensionGroupsJSONArray.length());
 
 		_assertExtensionGroupJSONObject(
-			"document-multimedia", "Audio",
+			"mp3", "document-multimedia", "Audio",
 			fileExtensionGroupsJSONArray.getJSONObject(0));
 		_assertExtensionGroupJSONObject(
-			"document-code", "Code",
+			"java", "document-code", "Code",
 			fileExtensionGroupsJSONArray.getJSONObject(1));
 		_assertExtensionGroupJSONObject(
-			"document-compressed", "Compressed",
+			"zip", "document-compressed", "Compressed",
 			fileExtensionGroupsJSONArray.getJSONObject(2));
 		_assertExtensionGroupJSONObject(
-			"document-image", "Image",
+			"jpg", "document-image", "Image",
 			fileExtensionGroupsJSONArray.getJSONObject(3));
 		_assertExtensionGroupJSONObject(
-			"document-presentation", "Presentation",
+			"ppt", "document-presentation", "Presentation",
 			fileExtensionGroupsJSONArray.getJSONObject(4));
 		_assertExtensionGroupJSONObject(
-			"document-table", "Spreadsheet",
+			"xls", "document-table", "Spreadsheet",
 			fileExtensionGroupsJSONArray.getJSONObject(5));
 		_assertExtensionGroupJSONObject(
-			"document-text", "Text",
+			"txt", "document-text", "Text",
 			fileExtensionGroupsJSONArray.getJSONObject(6));
 		_assertExtensionGroupJSONObject(
-			"document-pdf", "Vectorial",
+			"pdf", "document-pdf", "Vectorial",
 			fileExtensionGroupsJSONArray.getJSONObject(7));
 		_assertExtensionGroupJSONObject(
-			"document-multimedia", "Video",
+			"mp4", "document-multimedia", "Video",
 			fileExtensionGroupsJSONArray.getJSONObject(8));
 
 		Assert.assertNotNull(data.get("itemSelectorSaveEvent"));
 	}
 
+	@Test
+	public void testGetDataWithNoFileEntries() throws Exception {
+		Map<String, Object> data = _getData();
+
+		JSONArray fileExtensionGroupsJSONArray = (JSONArray)data.get(
+			"fileExtensionGroups");
+
+		Assert.assertEquals(0, fileExtensionGroupsJSONArray.length());
+
+		Assert.assertNotNull(data.get("itemSelectorSaveEvent"));
+	}
+
+	private FileEntry _addFileEntry(String fileExtension) throws Exception {
+		return DLAppLocalServiceUtil.addFileEntry(
+			RandomTestUtil.randomString(), TestPropsValues.getUserId(),
+			_group.getGroupId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString() + "." + fileExtension,
+			MimeTypesUtil.getExtensionContentType(fileExtension), new byte[0],
+			null, null,
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+	}
+
 	private void _assertExtensionGroupJSONObject(
-		String expectedIcon, String expectedLabel,
+		String expectedFileExtension, String expectedIcon, String expectedLabel,
 		JSONObject fileExtensionGroupJSONObject) {
 
 		Assert.assertEquals(
@@ -120,7 +160,14 @@ public class ContentDashboardFileExtensionItemSelectorViewTest {
 		JSONArray fileExtensionsJSONArray =
 			fileExtensionGroupJSONObject.getJSONArray("fileExtensions");
 
-		Assert.assertTrue(fileExtensionsJSONArray.length() > 0);
+		Assert.assertEquals(1, fileExtensionsJSONArray.length());
+
+		JSONObject fileExtensionJSONObject =
+			fileExtensionsJSONArray.getJSONObject(0);
+
+		Assert.assertEquals(
+			expectedFileExtension,
+			fileExtensionJSONObject.getString("fileExtension"));
 	}
 
 	private Map<String, Object> _getData() throws Exception {
@@ -141,8 +188,14 @@ public class ContentDashboardFileExtensionItemSelectorViewTest {
 			JavaConstants.JAVAX_PORTLET_RESPONSE,
 			new MockLiferayPortletRenderResponse());
 
+		ThemeDisplay themeDisplay = ContentDashboardTestUtil.getThemeDisplay(
+			_group);
+
+		themeDisplay.setRequest(mockHttpServletRequest);
+		themeDisplay.setUser(TestPropsValues.getUser());
+
 		mockHttpServletRequest.setAttribute(
-			WebKeys.THEME_DISPLAY, _getThemeDisplay());
+			WebKeys.THEME_DISPLAY, themeDisplay);
 
 		_contentDashboardFileExtensionItemSelectorView.renderHTML(
 			mockHttpServletRequest, new MockHttpServletResponse(), null,
@@ -157,15 +210,6 @@ public class ContentDashboardFileExtensionItemSelectorViewTest {
 		return ReflectionTestUtil.invoke(
 			contentDashboardFileExtensionItemSelectorViewDisplayContext,
 			"getData", new Class<?>[0], null);
-	}
-
-	private ThemeDisplay _getThemeDisplay() {
-		ThemeDisplay themeDisplay = new ThemeDisplay();
-
-		themeDisplay.setLocale(LocaleUtil.getDefault());
-		themeDisplay.setScopeGroupId(_group.getGroupId());
-
-		return themeDisplay;
 	}
 
 	@Inject(

commit 3d6d9946e9e88bd184e58b542c0770193a95dbf3

LPS-133354 Sorting again
I broke it in a01a5696075ec9c2dfc054731593850757b72aba.

They seem to come sorted from elasticsearch anyway. But just in case.

modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/selector/ContentDashboardFileExtensionItemSelectorView.java
@@ -218,7 +218,8 @@ public class ContentDashboardFileExtensionItemSelectorView
 					return JSONUtil.put(
 						"fileExtensions",
 						JSONUtil.putAll(
-							fileExtensionsStream.map(
+							fileExtensionsStream.sorted(
+							).map(
 								fileExtension -> JSONUtil.put(
 									"fileExtension", fileExtension
 								).put(