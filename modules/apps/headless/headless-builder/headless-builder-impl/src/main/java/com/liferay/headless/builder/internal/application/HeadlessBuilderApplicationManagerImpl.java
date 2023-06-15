7a4eafe3ca9773ec7a5f7b839502f7ca25cd7ef9
Luismi Barcos <luismiguel.barcos@liferay.com>
2023-06-15 19:11:06 +0200

    LPS-186786 Add Headless Builder Application Manager (interface and implementation). Also, add base classes to represent internal model of an application in the generator. Finally, adding some interfaces to abstract the future implementations

---
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/application/HeadlessBuilderApplicationManagerImpl.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.builder.internal.application;
+
+import com.liferay.headless.builder.application.HeadlessBuilderApplicationManager;
+import com.liferay.headless.builder.internal.generator.consumer.Consumer;
+import com.liferay.headless.builder.internal.generator.publisher.ApplicationPublisher;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luis Miguel Barcos
+ */
+@Component(service = HeadlessBuilderApplicationManager.class)
+public class HeadlessBuilderApplicationManagerImpl
+	implements HeadlessBuilderApplicationManager {
+
+	@Override
+	public void publishApplication(String applicationIdentifier)
+		throws Exception {
+
+		_applicationPublisher.publish(
+			_consumer.getApiApplication(applicationIdentifier));
+	}
+
+	@Override
+	public void unpublishApplication(String applicationIdentifier)
+		throws Exception {
+
+		_applicationPublisher.unpublish(
+			_consumer.getApiApplication(applicationIdentifier));
+	}
+
+	@Reference
+	private ApplicationPublisher _applicationPublisher;
+
+	@Reference
+	private Consumer _consumer;
+
+}
