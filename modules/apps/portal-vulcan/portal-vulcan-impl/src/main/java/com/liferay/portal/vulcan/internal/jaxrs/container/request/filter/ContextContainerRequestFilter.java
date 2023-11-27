4lejandrito/liferay-portal

commit 1c45a0cca19db122b29d7efc7bb601e48ee61288

LPS-166739 portal-vulcan-impl - previous implementation would set fields only for rest-builder Resources instead of all Resources which rely on VulcanFeature through Liferay.Vulcan extension

modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/container/request/filter/ContextContainerRequestFilter.java
@@ -155,11 +155,20 @@ public class ContextContainerRequestFilter implements ContainerRequestFilter {
 		_filterExcludedOperationIds(
 			containerRequestContext, httpServletRequest, message);
 
-		Class<?> clazz = instance.getClass();
+		_setInstanceFields(
+			instance.getClass(), httpServletRequest, message, instance);
+	}
 
-		Class<?> superClass = clazz.getSuperclass();
+	private void _setInstanceFields(
+			Class<?> clazz, HttpServletRequest httpServletRequest,
+			Message message, Object instance)
+		throws Exception {
 
-		for (Field field : superClass.getDeclaredFields()) {
+		if (clazz == Object.class) {
+			return;
+		}
+
+		for (Field field : clazz.getDeclaredFields()) {
 			if (Modifier.isFinal(field.getModifiers()) ||
 				Modifier.isStatic(field.getModifiers())) {
 
@@ -265,6 +274,9 @@ public class ContextContainerRequestFilter implements ContainerRequestFilter {
 
 				field.set(instance, _vulcanBatchEngineImportTaskResource);
 			}
+
+			_setInstanceFields(
+				clazz.getSuperclass(), httpServletRequest, message, instance);
 		}
 	}
 

commit 6a9326f6119bca0a8bcf344b3b68cbb2df9b840f

LPS-202760 Traversing the hierarchy in the right place
Only classes with at least 1 field where processing the super class.

Caused by 1c45a0cca19db122b29d7efc7bb601e48ee61288.

modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/container/request/filter/ContextContainerRequestFilter.java
@@ -390,10 +390,10 @@ public class ContextContainerRequestFilter implements ContainerRequestFilter {
 					instance,
 					_vulcanBatchEngineImportTaskResourceFactory.create());
 			}
-
-			_setInstanceFields(
-				clazz.getSuperclass(), httpServletRequest, message, instance);
 		}
+
+		_setInstanceFields(
+			clazz.getSuperclass(), httpServletRequest, message, instance);
 	}
 
 	private final ConfigurationAdmin _configurationAdmin;