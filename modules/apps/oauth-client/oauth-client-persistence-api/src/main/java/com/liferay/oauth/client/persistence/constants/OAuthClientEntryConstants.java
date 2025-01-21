de3744305236b65f23417c543cda22bbce1437c2
Brian Chan <brian.chan@liferay.com>
2025-01-21 15:54:45 +0900

    LPD-34901 Rename

--- modules/apps/oauth-client/oauth-client-persistence-api/src/main/java/com/liferay/oauth/client/persistence/constants/OAuthClientEntryConstants.java
+++ modules/apps/oauth-client/oauth-client-persistence-api/src/main/java/com/liferay/oauth/client/persistence/constants/OAuthClientEntryConstants.java
@@ -12,7 +12,7 @@ import com.liferay.portal.kernel.json.JSONUtil;
  */
 public class OAuthClientEntryConstants {
 
-	public static final long METADATA_CACHE_IN_MILLIS_DEFAULT = 360000;
+	public static final long METADATA_CACHE_TIME_DEFAULT = 360000;
 
 	public static final String OIDC_USER_INFO_MAPPER_JSON = JSONUtil.put(
 		"address",
