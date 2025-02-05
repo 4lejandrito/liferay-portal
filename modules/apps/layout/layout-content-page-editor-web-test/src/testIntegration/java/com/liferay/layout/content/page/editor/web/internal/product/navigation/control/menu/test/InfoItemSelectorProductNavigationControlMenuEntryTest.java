03d651e857e96e5589d71e387cade612a3584873
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-02-05 09:17:24 +0100

    LPD-48064 Sort by mockHttpServletRequest.*
    (cherry picked from commit 859953d61da98ce8cb6b3ba3d00cc73da3615b9b)

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/product/navigation/control/menu/test/InfoItemSelectorProductNavigationControlMenuEntryTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/product/navigation/control/menu/test/InfoItemSelectorProductNavigationControlMenuEntryTest.java
@@ -125,7 +125,6 @@ public class InfoItemSelectorProductNavigationControlMenuEntryTest {
 		MockHttpServletRequest mockHttpServletRequest =
 			new MockHttpServletRequest();
 
-		mockHttpServletRequest.setParameter("p_l_mode", Constants.EDIT);
 		mockHttpServletRequest.setAttribute(WebKeys.LAYOUT, layout);
 
 		ThemeDisplay themeDisplay = new ThemeDisplay();
@@ -141,6 +140,8 @@ public class InfoItemSelectorProductNavigationControlMenuEntryTest {
 
 		mockHttpServletRequest.setAttribute(
 			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		mockHttpServletRequest.setParameter("p_l_mode", Constants.EDIT);
 
 		return mockHttpServletRequest;
 	}
