1fde17f0fdbc5de37d878293c377091059e084a2
Evan Thibodeau <evan.thibodeau@liferay.com>
2025-02-21 12:52:20 +0400

    LPD-49070 - Add tests for adding "Show Control Menu by Role" configuration via site initializer.

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
@@ -206,6 +206,7 @@ import com.liferay.segments.model.SegmentsEntry;
 import com.liferay.segments.model.SegmentsExperience;
 import com.liferay.segments.service.SegmentsEntryLocalService;
 import com.liferay.segments.service.SegmentsExperienceLocalService;
+import com.liferay.site.configuration.manager.MenuAccessConfigurationManager;
 import com.liferay.site.initializer.SiteInitializer;
 import com.liferay.site.initializer.SiteInitializerFactory;
 import com.liferay.site.initializer.SiteInitializerRegistry;
@@ -3756,12 +3757,30 @@ public class BundleSiteInitializerTest {
 			segmentsExperience2.getSegmentsExperienceId());
 	}
 
-	private void _assertSiteConfiguration() {
+	private void _assertSiteConfiguration1() throws Exception {
 		Assert.assertEquals(
 			GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION,
 			_group.getMembershipRestriction());
 		Assert.assertEquals(GroupConstants.TYPE_SITE_OPEN, _group.getType());
 		Assert.assertTrue(_group.isManualMembership());
+
+		Assert.assertTrue(
+			_menuAccessConfigurationManager.isShowControlMenuByRole(
+				_group.getGroupId()));
+		Assert.assertEquals(
+			2,
+			_menuAccessConfigurationManager.getAccessToControlMenuRoleIds(
+				_group.getGroupId()).length);
+	}
+
+	private void _assertSiteConfiguration2() throws Exception {
+		Assert.assertFalse(
+			_menuAccessConfigurationManager.isShowControlMenuByRole(
+				_group.getGroupId()));
+		Assert.assertEquals(
+			0,
+			_menuAccessConfigurationManager.getAccessToControlMenuRoleIds(
+				_group.getGroupId()).length);
 	}
 
 	private void _assertSiteNavigationMenu1() {
@@ -4442,7 +4461,7 @@ public class BundleSiteInitializerTest {
 		_assertPortletSettings();
 		_assertSAPEntries();
 		_assertSegmentsEntries();
-		_assertSiteConfiguration();
+		_assertSiteConfiguration1();
 		_assertSiteSettings();
 		_assertSiteNavigationMenu1();
 		_assertStyleBookEntry();
@@ -4481,6 +4500,7 @@ public class BundleSiteInitializerTest {
 		_assertOrganizations2();
 		_assertPLOEntries2();
 		_assertResourcePermission2();
+		_assertSiteConfiguration2();
 		_assertSiteNavigationMenu2();
 		_assertSXPBlueprint2();
 		_assertUserAccounts2();
@@ -4636,6 +4656,9 @@ public class BundleSiteInitializerTest {
 	@Inject
 	private ListTypeDefinitionResource.Factory
 		_listTypeDefinitionResourceFactory;
+
+	@Inject
+	private MenuAccessConfigurationManager _menuAccessConfigurationManager;
 
 	@Inject
 	private NotificationTemplateResource.Factory
