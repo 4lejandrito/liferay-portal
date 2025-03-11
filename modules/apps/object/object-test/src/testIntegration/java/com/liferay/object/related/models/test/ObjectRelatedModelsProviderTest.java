6125dc24cd8b830b3981b89fb9cd75adec673e0c
Brian Chan <brian.chan@liferay.com>
2025-03-11 11:08:30 -0300

    LPD-47681 SF

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/related/models/test/ObjectRelatedModelsProviderTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/related/models/test/ObjectRelatedModelsProviderTest.java
@@ -826,9 +826,6 @@ public class ObjectRelatedModelsProviderTest {
 			_objectRelationshipLocalService.deleteObjectRelationship(
 				_objectRelationship.getObjectRelationshipId());
 
-			_assetEntryLocalService.deleteEntry(
-				objectDefinition.getClassName(), objectEntry.getPrimaryKey());
-
 			_objectDefinitionLocalService.deleteObjectDefinition(
 				objectDefinition);
 
@@ -837,6 +834,9 @@ public class ObjectRelatedModelsProviderTest {
 					accountEntry1.getAccountEntryId(),
 					accountEntry2.getAccountEntryId()
 				});
+
+			_assetEntryLocalService.deleteEntry(
+				objectDefinition.getClassName(), objectEntry.getPrimaryKey());
 		}
 		finally {
 			PermissionThreadLocal.setPermissionChecker(
