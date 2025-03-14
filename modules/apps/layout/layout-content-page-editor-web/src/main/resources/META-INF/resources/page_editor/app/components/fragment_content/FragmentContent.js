b669a6cb1b2931d5c95b0c0d535c17af60a1919a
Sandro Chinea <sandrochinea@gmail.com>
2025-03-14 08:50:31 +0000

    LPD-51297 Show old topper for embedded widgets

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment_content/FragmentContent.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment_content/FragmentContent.js
@@ -120,8 +120,10 @@ const FragmentContent = ({
 
 	const cssClasses = getLayoutDataItemCssClasses(item);
 
-	const portletCustomActions = useMemo(
-		() => getPortletCustomActions(fragmentEntryLink),
+	const showPortletTopper = useMemo(
+		() =>
+			getPortletCustomActions(fragmentEntryLink).length ||
+			fragmentEntryLink.fragmentEntryType !== 'widget',
 		[fragmentEntryLink]
 	);
 
@@ -269,8 +271,7 @@ const FragmentContent = ({
 								!hasInnerCommonStyles(fragmentEntryLink),
 							'custom-height': item.config.styles?.height,
 							'page-editor__fragment-content--portlet-topper-hidden':
-								!canConfigureWidgets ||
-								!portletCustomActions.length,
+								!canConfigureWidgets || !showPortletTopper,
 						}
 					)}
 					contentRef={elementRef}
