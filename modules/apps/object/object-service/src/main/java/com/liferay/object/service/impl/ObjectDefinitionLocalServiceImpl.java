a96803f9bcf5ab8a98f0f6546eceaf9ec23eee1e
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-03-27 08:23:52 -0300

    LPD-47681 Update fix

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -605,6 +605,11 @@ public class ObjectDefinitionLocalServiceImpl
 
 			undeployObjectDefinition(objectDefinition);
 
+			ClassName className = _classNameLocalService.getClassName(
+				objectDefinition.getClassName());
+
+			_classNameLocalService.deleteClassName(className);
+
 			_registerTransactionCallbackForCluster(
 				_undeployObjectDefinitionMethodKey, objectDefinition);
 		}
@@ -1867,12 +1872,11 @@ public class ObjectDefinitionLocalServiceImpl
 	}
 
 	private void _invalidatePortalCache(ObjectDefinition objectDefinition) {
-		ClassName className = _classNameLocalService.getClassName(
-			objectDefinition.getClassName());
-
 		List<LayoutClassedModelUsage> layoutClassedModelUsages =
 			_layoutClassedModelUsageLocalService.getLayoutClassedModelUsages(
-				objectDefinition.getCompanyId(), className.getClassNameId(),
+				objectDefinition.getCompanyId(),
+				_classNameLocalService.getClassNameId(
+					objectDefinition.getClassName()),
 				_portal.getClassNameId(FragmentEntryLink.class));
 
 		for (LayoutClassedModelUsage layoutClassedModelUsage :
@@ -1881,8 +1885,6 @@ public class ObjectDefinitionLocalServiceImpl
 			_fragmentEntryLinkCache.removeFragmentEntryLinkCache(
 				GetterUtil.getLong(layoutClassedModelUsage.getContainerKey()));
 		}
-
-		_classNameLocalService.deleteClassName(className);
 	}
 
 	private boolean _isUnmodifiableSystemObject(
