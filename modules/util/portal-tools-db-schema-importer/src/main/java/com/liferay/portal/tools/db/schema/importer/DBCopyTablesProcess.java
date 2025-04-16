f53a573f074d78f6fa7311edd7933abd8a68f9a3
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-04-16 19:50:15 +0200

    LPD-53545 BigDecimal columns in PostgreSQL can be considered as Types.NUMERIC. Then there is no need to transform BigDecimal to String back then to BigDecimal. Perform the conversion directly.

--- modules/util/portal-tools-db-schema-importer/src/main/java/com/liferay/portal/tools/db/schema/importer/DBCopyTablesProcess.java
+++ modules/util/portal-tools-db-schema-importer/src/main/java/com/liferay/portal/tools/db/schema/importer/DBCopyTablesProcess.java
@@ -332,7 +332,9 @@ public class DBCopyTablesProcess {
 				return;
 			}
 
-			if (targetType == Types.DECIMAL) {
+			if ((targetType == Types.DECIMAL) ||
+				(targetType == Types.NUMERIC)) {
+
 				preparedStatement.setBigDecimal(index, value);
 
 				return;
@@ -521,7 +523,7 @@ public class DBCopyTablesProcess {
 			String value)
 		throws Exception {
 
-		if ((targetType == Types.BIGINT) || (targetType == Types.NUMERIC)) {
+		if (targetType == Types.BIGINT) {
 			preparedStatement.setLong(index, GetterUtil.getLong(value));
 		}
 		else if ((targetType == Types.BIT) || (targetType == Types.BOOLEAN)) {
@@ -551,6 +553,17 @@ public class DBCopyTablesProcess {
 		}
 		else if (targetType == Types.INTEGER) {
 			preparedStatement.setInt(index, GetterUtil.getInteger(value));
+		}
+		else if (targetType == Types.NUMERIC) {
+			Number number = GetterUtil.getNumber(value);
+
+			if (number instanceof Long) {
+				preparedStatement.setLong(index, (Long)number);
+			}
+			else {
+				preparedStatement.setBigDecimal(
+					index, (BigDecimal)GetterUtil.get(value, BigDecimal.ZERO));
+			}
 		}
 		else if ((targetType == Types.SMALLINT) ||
 				 (targetType == Types.TINYINT)) {
