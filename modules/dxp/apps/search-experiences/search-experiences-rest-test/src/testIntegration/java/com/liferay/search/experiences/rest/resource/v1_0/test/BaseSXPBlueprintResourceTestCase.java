48fdf90a712bb03e2ce6523627a4b4ee1791fc18
Daniel Raposo <daniel.raposo@liferay.com>
2025-05-29 13:29:29 +0200

    LPD-46121 buildREST

--- modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/BaseSXPBlueprintResourceTestCase.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/BaseSXPBlueprintResourceTestCase.java
@@ -346,7 +346,7 @@ public abstract class BaseSXPBlueprintResourceTestCase {
 			testDeleteSXPBlueprintBatch_addSXPBlueprint();
 
 		testDeleteSXPBlueprintBatch_deleteSXPBlueprint(
-			"COMPLETED", null, sxpBlueprint1.getId());
+			202, null, sxpBlueprint1.getId());
 
 		assertHttpResponseStatusCode(
 			404,
@@ -361,7 +361,7 @@ public abstract class BaseSXPBlueprintResourceTestCase {
 	}
 
 	protected void testDeleteSXPBlueprintBatch_deleteSXPBlueprint(
-			String expectedExecuteStatus, String externalReferenceCode, Long id)
+			int expectedStatusCode, String externalReferenceCode, Long id)
 		throws Exception {
 
 		HttpInvoker.HttpResponse httpResponse =
@@ -374,11 +374,13 @@ public abstract class BaseSXPBlueprintResourceTestCase {
 						"id", () -> id
 					)));
 
-		Assert.assertEquals(202, httpResponse.getStatusCode());
+		Assert.assertEquals(expectedStatusCode, httpResponse.getStatusCode());
 
-		waitForFinish(
-			expectedExecuteStatus,
-			JSONFactoryUtil.createJSONObject(httpResponse.getContent()));
+		if (expectedStatusCode == 202) {
+			waitForFinish(
+				"COMPLETED",
+				JSONFactoryUtil.createJSONObject(httpResponse.getContent()));
+		}
 	}
 
 	@Test
