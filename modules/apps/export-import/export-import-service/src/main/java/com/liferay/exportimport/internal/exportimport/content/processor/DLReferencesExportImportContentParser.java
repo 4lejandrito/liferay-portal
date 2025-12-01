6569a3655c05e71fc547e75f374512e295064e45
Carlos Correa <carlos.correa@liferay.com>
2025-12-01 12:06:43 +0100

    LPD-72635 Add method to process the dependencies

---
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/exportimport/content/processor/DLReferencesExportImportContentParser.java
@@ -0,0 +1,179 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.exportimport.internal.exportimport.content.processor;
+
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.exportimport.content.processor.ExportImportContentParser;
+import com.liferay.exportimport.content.processor.constants.ExportImportContentParserConstants;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.repository.friendly.url.resolver.FileEntryFriendlyURLResolver;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carlos Correa
+ */
+@Component(
+	property = ExportImportContentParserConstants.CONTENT_PARSER_TYPE + "=" + ExportImportContentParserConstants.DOCUMENT_LIBRARY,
+	service = ExportImportContentParser.class
+)
+public class DLReferencesExportImportContentParser
+	implements ExportImportContentParser {
+
+	@Override
+	public String parseExportContent(
+		String content, PortletDataContext portletDataContext) {
+
+		StringBuilder sb = new StringBuilder(content);
+
+		DLReferencesReverseIterator dlReferencesReverseIterator =
+			new DLReferencesReverseIterator(
+				content, _fileEntryFriendlyURLResolver,
+				portletDataContext.getScopeGroupId());
+
+		while (dlReferencesReverseIterator.hasNext()) {
+			DLReferencesReverseIterator.DLReference dlReference =
+				dlReferencesReverseIterator.next();
+
+			FileEntry fileEntry = dlReference.getFileEntry();
+
+			if (fileEntry == null) {
+				continue;
+			}
+
+			DocumentLibraryReference documentLibraryReference =
+				new DocumentLibraryReference(
+					fileEntry, dlReference.getFriendlyURL());
+
+			sb.replace(
+				dlReference.getBeginPos(), dlReference.getEndPos(),
+				documentLibraryReference.toString());
+		}
+
+		return sb.toString();
+	}
+
+	@Override
+	public String parseImportContent(
+			String content, PortletDataContext portletDataContext)
+		throws Exception {
+
+		StringBuilder sb = new StringBuilder(content);
+
+		DocumentLibraryReference documentLibraryReference = null;
+
+		while (
+			(documentLibraryReference = DocumentLibraryReference.parse(content)) != null) {
+
+			FileEntry fileEntry =
+				_dlAppLocalService.fetchFileEntryByExternalReferenceCode(
+					portletDataContext.getScopeGroupId(),
+					documentLibraryReference.getExternalReferenceCode());
+
+			if (fileEntry == null) {
+
+
+			}
+		}
+
+		return content;
+	}
+
+
+	@Reference
+	private DLAppLocalService _dlAppLocalService;
+
+	@Reference
+	private FileEntryFriendlyURLResolver _fileEntryFriendlyURLResolver;
+
+	private static class DocumentLibraryReference {
+
+		public static DocumentLibraryReference parse(String value) {
+			int lastIndex = value.lastIndexOf("[$dl-reference$");
+
+			if (lastIndex == -1) {
+				return null;
+			}
+
+			value = value.substring(lastIndex);
+
+			String externalReferenceCode = value.substring(
+				value.indexOf("$dl-external-reference-code=") +
+					"$dl-external-reference-code=".length(),
+				value.indexOf("$,$dl-group-id="));
+			long groupId = GetterUtil.getLong(
+				value.substring(
+					value.indexOf("$,$dl-group-id=") +
+						"$,$dl-group-id=".length(),
+					value.indexOf("$,$friendly-url=")));
+			String friendlyURL = value.substring(
+				value.indexOf("$,$friendly-url=") + "$,$friendly-url=".length(),
+				value.indexOf("$,$uuid="));
+			String uuid = value.substring(
+				value.indexOf("$,$uuid=") + "$,$uuid=".length(),
+				value.indexOf("$]"));
+
+			return new DocumentLibraryReference(
+				externalReferenceCode, friendlyURL, groupId, uuid);
+		}
+
+		public DocumentLibraryReference(
+			FileEntry fileEntry, String friendlyURL) {
+
+			_friendlyURL = friendlyURL;
+
+			_externalReferenceCode = fileEntry.getExternalReferenceCode();
+			_groupId = fileEntry.getGroupId();
+			_uuid = fileEntry.getUuid();
+		}
+
+		public String getExternalReferenceCode() {
+			return _externalReferenceCode;
+		}
+
+		public String getFriendlyURL() {
+			return _friendlyURL;
+		}
+
+		public long getGroupId() {
+			return _groupId;
+		}
+
+		public String getUuid() {
+			return _uuid;
+		}
+
+		@Override
+		public String toString() {
+			return StringBundler.concat(
+				"[$dl-reference$ $dl-external-reference-code=",
+				_externalReferenceCode, "$,$dl-group-id=", _groupId,
+				"$,$friendly-url=", _friendlyURL, "$,$uuid=", _uuid, "$]");
+		}
+
+		private DocumentLibraryReference(
+			String externalReferenceCode, String friendlyURL, long groupId,
+			String uuid) {
+
+			_externalReferenceCode = externalReferenceCode;
+			_friendlyURL = friendlyURL;
+			_groupId = groupId;
+			_uuid = uuid;
+		}
+
+		private final String _externalReferenceCode;
+		private final String _friendlyURL;
+		private final long _groupId;
+		private final String _uuid;
+
+	}
+
+}
