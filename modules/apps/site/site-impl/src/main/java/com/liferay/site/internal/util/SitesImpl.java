4lejandrito/liferay-portal

commit ab3cf74ea42dd353e4c128e40bdff1e8220d5f0e

LPD-81592 Remove ready for propagation feature from site templates
The readyForPropagation toggle allowed admins to control when site
template changes propagated to connected sites. This completely
removes the feature from the UI, the service layer, and adds an
upgrade process to remove it for existing site templates.

modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
@@ -459,18 +459,6 @@ public class SitesImpl implements Sites {
 			return false;
 		}
 
-		UnicodeProperties layoutSetPrototypeSettingsUnicodeProperties =
-			layoutSetPrototype.getSettingsProperties();
-
-		boolean readyForPropagation = GetterUtil.getBoolean(
-			layoutSetPrototypeSettingsUnicodeProperties.getProperty(
-				"readyForPropagation"),
-			true);
-
-		if (!readyForPropagation && !(lastMergeTime == 0)) {
-			return false;
-		}
-
 		LayoutSet layoutSetPrototypeLayoutSet =
 			layoutSetPrototype.getLayoutSet();
 

modules/apps/depot/depot-service/src/main/java/com/liferay/depot/service/impl/DepotEntryGroupRelLocalServiceImpl.java
@@ -12,20 +12,10 @@ import com.liferay.depot.service.base.DepotEntryGroupRelLocalServiceBaseImpl;
 import com.liferay.depot.service.persistence.DepotEntryPersistence;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.LayoutSet;
-import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.model.SystemEventConstants;
-import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.service.LayoutSetLocalService;
-import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.UnicodeProperties;
 
 import java.util.List;
 
@@ -53,12 +43,6 @@ public class DepotEntryGroupRelLocalServiceImpl
 
 		if (depotEntryGroupRel != null) {
 			return depotEntryGroupRel;
-		}
-
-		Group toGroup = _groupLocalService.getGroup(toGroupId);
-
-		if (toGroup.isLayoutSetPrototype()) {
-			_connectLayoutSetPrototypeGroups(depotEntryId, toGroup);
 		}
 
 		depotEntryGroupRel = depotEntryGroupRelPersistence.create(
@@ -107,14 +91,6 @@ public class DepotEntryGroupRelLocalServiceImpl
 	public DepotEntryGroupRel deleteDepotEntryGroupRel(
 			DepotEntryGroupRel depotEntryGroupRel)
 		throws PortalException {
-
-		Group toGroup = _groupLocalService.getGroup(
-			depotEntryGroupRel.getToGroupId());
-
-		if (toGroup.isLayoutSetPrototype()) {
-			_disconnectLayoutSetPrototypeGroups(
-				depotEntryGroupRel.getDepotEntryId(), toGroup);
-		}
 
 		return super.deleteDepotEntryGroupRel(depotEntryGroupRel);
 	}
@@ -232,92 +208,7 @@ public class DepotEntryGroupRelLocalServiceImpl
 		return depotEntryGroupRelPersistence.update(depotEntryGroupRel);
 	}
 
-	private void _connectLayoutSetPrototypeGroups(
-			long depotEntryId, Group toGroup)
-		throws PortalException {
-
-		LayoutSetPrototype layoutSetPrototype =
-			_layoutSetPrototypeLocalService.getLayoutSetPrototype(
-				toGroup.getClassPK());
-
-		UnicodeProperties settingsUnicodeProperties =
-			layoutSetPrototype.getSettingsProperties();
-
-		boolean readyForPropagation = GetterUtil.getBoolean(
-			settingsUnicodeProperties.getProperty("readyForPropagation"));
-
-		if (!readyForPropagation) {
-			return;
-		}
-
-		List<LayoutSet> layoutSets =
-			_layoutSetLocalService.getLayoutSetsByLayoutSetPrototypeUuid(
-				layoutSetPrototype.getUuid());
-
-		for (LayoutSet layoutSet : layoutSets) {
-			addDepotEntryGroupRel(depotEntryId, layoutSet.getGroupId());
-		}
-	}
-
-	private void _disconnectLayoutSetPrototypeGroups(
-			long depotEntryId, Group group)
-		throws PortalException {
-
-		LayoutSetPrototype layoutSetPrototype =
-			_layoutSetPrototypeLocalService.getLayoutSetPrototype(
-				group.getClassPK());
-
-		UnicodeProperties settingsUnicodeProperties =
-			layoutSetPrototype.getSettingsProperties();
-
-		boolean readyForPropagation = GetterUtil.getBoolean(
-			settingsUnicodeProperties.getProperty("readyForPropagation"));
-
-		if (!readyForPropagation) {
-			return;
-		}
-
-		List<LayoutSet> layoutSets =
-			_layoutSetLocalService.getLayoutSetsByLayoutSetPrototypeUuid(
-				layoutSetPrototype.getUuid());
-
-		for (LayoutSet layoutSet : layoutSets) {
-			DepotEntryGroupRel depotEntryGroupRel =
-				fetchDepotEntryGroupRelByDepotEntryIdToGroupId(
-					depotEntryId, layoutSet.getGroupId());
-
-			if (depotEntryGroupRel == null) {
-				continue;
-			}
-
-			if (depotEntryGroupRel.isDdmStructuresAvailable()) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to disconnect " +
-							depotEntryGroupRel.getToGroupId());
-				}
-
-				continue;
-			}
-
-			depotEntryGroupRelLocalService.deleteDepotEntryGroupRel(
-				depotEntryGroupRel);
-		}
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		DepotEntryGroupRelLocalServiceImpl.class);
-
 	@Reference
 	private DepotEntryPersistence _depotEntryPersistence;
-
-	@Reference
-	private GroupLocalService _groupLocalService;
-
-	@Reference
-	private LayoutSetLocalService _layoutSetLocalService;
-
-	@Reference
-	private LayoutSetPrototypeLocalService _layoutSetPrototypeLocalService;
 
 }

modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/service/test/DepotEntryGroupRelLocalServiceTest.java
@@ -132,8 +132,7 @@ public class DepotEntryGroupRelLocalServiceTest {
 				HashMapBuilder.put(
 					LocaleUtil.getDefault(), RandomTestUtil.randomString()
 				).build(),
-				null, true, true, false,
-				ServiceContextTestUtil.getServiceContext());
+				null, true, true, ServiceContextTestUtil.getServiceContext());
 
 		Group group = _setUpLayoutSetPrototypeGroup(layoutSetPrototype);
 
@@ -237,8 +236,7 @@ public class DepotEntryGroupRelLocalServiceTest {
 			HashMapBuilder.put(
 				LocaleUtil.getDefault(), RandomTestUtil.randomString()
 			).build(),
-			null, true, true, false,
-			ServiceContextTestUtil.getServiceContext());
+			null, true, true, ServiceContextTestUtil.getServiceContext());
 
 		int systemEventsCount = _systemEventLocalService.getSystemEventsCount();
 

modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/service/DefaultLayoutLayoutSetPrototypeLocalServiceWrapper.java
@@ -34,13 +34,12 @@ public class DefaultLayoutLayoutSetPrototypeLocalServiceWrapper
 	public LayoutSetPrototype addLayoutSetPrototype(
 			long userId, long companyId, Map<Locale, String> nameMap,
 			Map<Locale, String> descriptionMap, boolean active,
-			boolean layoutsUpdateable, boolean readyForPropagation,
-			ServiceContext serviceContext)
+			boolean layoutsUpdateable, ServiceContext serviceContext)
 		throws PortalException {
 
 		LayoutSetPrototype layoutSetPrototype = super.addLayoutSetPrototype(
 			userId, companyId, nameMap, descriptionMap, active,
-			layoutsUpdateable, readyForPropagation, serviceContext);
+			layoutsUpdateable, serviceContext);
 
 		if (GetterUtil.getBoolean(
 				serviceContext.getAttribute("addDefaultLayout"), true)) {
@@ -65,18 +64,6 @@ public class DefaultLayoutLayoutSetPrototypeLocalServiceWrapper
 		}
 
 		return layoutSetPrototype;
-	}
-
-	@Override
-	public LayoutSetPrototype addLayoutSetPrototype(
-			long userId, long companyId, Map<Locale, String> nameMap,
-			Map<Locale, String> descriptionMap, boolean active,
-			boolean layoutsUpdateable, ServiceContext serviceContext)
-		throws PortalException {
-
-		return addLayoutSetPrototype(
-			userId, companyId, nameMap, descriptionMap, active,
-			layoutsUpdateable, true, serviceContext);
 	}
 
 	@Reference

modules/apps/layout/layout-set-prototype-api/src/main/java/com/liferay/layout/set/prototype/configuration/LayoutSetPrototypeConfiguration.java
@@ -30,10 +30,4 @@ public interface LayoutSetPrototypeConfiguration {
 	)
 	public boolean cancelPropagationImportTask();
 
-	@Meta.AD(
-		deflt = "false", description = "trigger-propagation-help",
-		name = "trigger-propagation", required = false
-	)
-	public boolean triggerPropagation();
-
 }

modules/apps/layout/layout-set-prototype-impl/src/main/java/com/liferay/layout/set/prototype/internal/exportimport/data/handler/LayoutSetPrototypeStagedModelDataHandler.java
@@ -159,8 +159,6 @@ public class LayoutSetPrototypeStagedModelDataHandler
 
 		boolean layoutsUpdateable = GetterUtil.getBoolean(
 			settingsUnicodeProperties.getProperty("layoutsUpdateable"), true);
-		boolean readyForPropagation = GetterUtil.getBoolean(
-			settingsUnicodeProperties.getProperty("readyForPropagation"), true);
 
 		ServiceContext serviceContext = portletDataContext.createServiceContext(
 			layoutSetPrototype);
@@ -185,7 +183,7 @@ public class LayoutSetPrototypeStagedModelDataHandler
 						layoutSetPrototype.getNameMap(),
 						layoutSetPrototype.getDescriptionMap(),
 						layoutSetPrototype.isActive(), layoutsUpdateable,
-						readyForPropagation, serviceContext);
+						serviceContext);
 			}
 			else {
 				importedLayoutSetPrototype =
@@ -194,7 +192,7 @@ public class LayoutSetPrototypeStagedModelDataHandler
 						layoutSetPrototype.getNameMap(),
 						layoutSetPrototype.getDescriptionMap(),
 						layoutSetPrototype.isActive(), layoutsUpdateable,
-						readyForPropagation, serviceContext);
+						serviceContext);
 			}
 		}
 		else {
@@ -204,7 +202,7 @@ public class LayoutSetPrototypeStagedModelDataHandler
 					layoutSetPrototype.getNameMap(),
 					layoutSetPrototype.getDescriptionMap(),
 					layoutSetPrototype.isActive(), layoutsUpdateable,
-					readyForPropagation, serviceContext);
+					serviceContext);
 		}
 
 		_importLayoutPrototypes(portletDataContext, layoutSetPrototype);

modules/apps/layout/layout-set-prototype-test/src/testIntegration/java/com/liferay/layout/set/prototype/internal/product/navigation/control/menu/test/PropagationMessageProductNavigationControlMenuEntryTest.java
@@ -1,107 +1,0 @@
-/**
- * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
-
-package com.liferay.layout.set.prototype.internal.product.navigation.control.menu.test;
-
-import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.layout.test.util.LayoutTestUtil;
-import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.LayoutSetPrototype;
-import com.liferay.portal.kernel.model.LayoutTypePortlet;
-import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.service.CompanyLocalService;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.test.rule.Inject;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
-import com.liferay.product.navigation.control.menu.ProductNavigationControlMenuEntry;
-
-import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.mock.web.MockHttpServletRequest;
-
-/**
- * @author Eudaldo Alonso
- */
-@RunWith(Arquillian.class)
-public class PropagationMessageProductNavigationControlMenuEntryTest {
-
-	@ClassRule
-	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			PermissionCheckerMethodTestRule.INSTANCE);
-
-	@Test
-	public void testIsShowForUSerWithoutPermission() throws Exception {
-		LayoutSetPrototype layoutSetPrototype =
-			LayoutTestUtil.addLayoutSetPrototype(RandomTestUtil.randomString());
-
-		PermissionChecker permissionChecker =
-			PermissionThreadLocal.getPermissionChecker();
-
-		PermissionThreadLocal.setPermissionChecker(
-			PermissionCheckerFactoryUtil.create(UserTestUtil.addUser()));
-
-		try {
-			Assert.assertFalse(
-				_productNavigationControlMenuEntry.isShow(
-					_getMockHttpServletRequest(
-						LayoutTestUtil.addTypePortletLayout(
-							layoutSetPrototype.getGroup()))));
-		}
-		finally {
-			PermissionThreadLocal.setPermissionChecker(permissionChecker);
-		}
-	}
-
-	private MockHttpServletRequest _getMockHttpServletRequest(Layout layout)
-		throws Exception {
-
-		MockHttpServletRequest mockHttpServletRequest =
-			new MockHttpServletRequest();
-
-		mockHttpServletRequest.setAttribute(
-			WebKeys.THEME_DISPLAY, _getThemeDisplay(layout));
-
-		return mockHttpServletRequest;
-	}
-
-	private ThemeDisplay _getThemeDisplay(Layout layout) throws Exception {
-		ThemeDisplay themeDisplay = new ThemeDisplay();
-
-		themeDisplay.setCompany(
-			_companyLocalService.getCompany(TestPropsValues.getCompanyId()));
-		themeDisplay.setLayout(layout);
-		themeDisplay.setLayoutTypePortlet(
-			(LayoutTypePortlet)layout.getLayoutType());
-		themeDisplay.setPermissionChecker(
-			PermissionThreadLocal.getPermissionChecker());
-
-		return themeDisplay;
-	}
-
-	@Inject
-	private CompanyLocalService _companyLocalService;
-
-	@Inject(
-		filter = "component.name=com.liferay.layout.set.prototype.web.internal.product.navigation.control.menu.PropagationMessageProductNavigationControlMenuEntry"
-	)
-	private ProductNavigationControlMenuEntry
-		_productNavigationControlMenuEntry;
-
-}

modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/display/context/PropagationMessageDisplayContext.java
@@ -1,111 +1,0 @@
-/**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
-
-package com.liferay.layout.set.prototype.web.internal.display.context;
-
-import com.liferay.layout.set.prototype.configuration.LayoutSetPrototypeConfiguration;
-import com.liferay.layout.set.prototype.constants.LayoutSetPrototypePortletKeys;
-import com.liferay.portal.configuration.module.configuration.ConfigurationProviderUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.LayoutSetPrototype;
-import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
-import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
-import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalServiceUtil;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.UnicodeProperties;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import jakarta.portlet.PortletRequest;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-import java.util.Map;
-
-/**
- * @author Tamas Molnar
- */
-public class PropagationMessageDisplayContext {
-
-	public PropagationMessageDisplayContext(
-		HttpServletRequest httpServletRequest) {
-
-		_httpServletRequest = httpServletRequest;
-	}
-
-	public Map<String, Object> getData() {
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)_httpServletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
-		Layout layout = themeDisplay.getLayout();
-
-		Group group = layout.getGroup();
-
-		LayoutSetPrototype layoutSetPrototype =
-			LayoutSetPrototypeLocalServiceUtil.fetchLayoutSetPrototype(
-				group.getClassPK());
-
-		UnicodeProperties settingsUnicodeProperties =
-			layoutSetPrototype.getSettingsProperties();
-
-		boolean readyForPropagation = GetterUtil.getBoolean(
-			settingsUnicodeProperties.getProperty("readyForPropagation"));
-
-		return HashMapBuilder.<String, Object>put(
-			"enableDisablePropagationURL",
-			() -> PortletURLBuilder.create(
-				PortletURLFactoryUtil.create(
-					_httpServletRequest,
-					LayoutSetPrototypePortletKeys.LAYOUT_SET_PROTOTYPE,
-					PortletRequest.ACTION_PHASE)
-			).setActionName(
-				"updateLayoutSetPrototypeAction"
-			).setRedirect(
-				PortalUtil.getLayoutURL(themeDisplay)
-			).setParameter(
-				"layoutSetPrototypeId",
-				layoutSetPrototype.getLayoutSetPrototypeId()
-			).setParameter(
-				"readyForPropagation", !readyForPropagation
-			).buildString()
-		).put(
-			"portletNamespace",
-			PortalUtil.getPortletNamespace(
-				LayoutSetPrototypePortletKeys.LAYOUT_SET_PROTOTYPE)
-		).put(
-			"readyForPropagation", readyForPropagation
-		).put(
-			"triggerPropagation",
-			isTriggerPropagation(themeDisplay.getCompanyId())
-		).build();
-	}
-
-	protected boolean isTriggerPropagation(long companyId) {
-		try {
-			LayoutSetPrototypeConfiguration layoutSetPrototypeConfiguration =
-				ConfigurationProviderUtil.getCompanyConfiguration(
-					LayoutSetPrototypeConfiguration.class, companyId);
-
-			return layoutSetPrototypeConfiguration.triggerPropagation();
-		}
-		catch (Exception exception) {
-			_log.error(exception);
-		}
-
-		return false;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		PropagationMessageDisplayContext.class);
-
-	private final HttpServletRequest _httpServletRequest;
-
-}

commit 11da156576084c5f0bdf4f136e44f077c334b5b8

LPD-81591 This if was wrong. My bad
Caused by ab3cf74ea42dd353e4c128e40bdff1e8220d5f0e. When ready for
propagation was disabled the propagation had to happen only the first
time. With the previous change I allowed it in every case, going against
the whole point of the change.

Sorry.

modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
@@ -459,6 +459,10 @@ public class SitesImpl implements Sites {
 			return false;
 		}
 
+		if (lastMergeTime != 0) {
+			return false;
+		}
+
 		LayoutSet layoutSetPrototypeLayoutSet =
 			layoutSetPrototype.getLayoutSet();
 