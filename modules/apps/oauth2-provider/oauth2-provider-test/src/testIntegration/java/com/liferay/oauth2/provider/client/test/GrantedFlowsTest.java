73f70a1994aab5b50bb1af811f226e34982a1ae2
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-14 15:46:07 +0200

    LPD-87965 Replace hardcoded 8080 in GrantedFlowsTest

--- modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/GrantedFlowsTest.java
+++ modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/GrantedFlowsTest.java
@@ -11,6 +11,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PropsValues;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
@@ -126,7 +127,7 @@ public class GrantedFlowsTest extends BaseClientTestCase {
 			createOAuth2ApplicationWithNone(
 				companyId, _user, "oauthTestApplicationCodePKCE",
 				Collections.singletonList(GrantType.AUTHORIZATION_CODE_PKCE),
-				Collections.singletonList("http://redirecturi:8080"), false,
+				Collections.singletonList("http://redirecturi:" + PortalUtil.getPortalServerPort(false)), false,
 				Collections.singletonList("everything"), false);
 
 			createOAuth2Application(
@@ -137,7 +138,7 @@ public class GrantedFlowsTest extends BaseClientTestCase {
 			createOAuth2ApplicationWithNone(
 				companyId, _user, "oauthTestApplicationNoGrants",
 				Collections.emptyList(),
-				Collections.singletonList("http://redirecturi:8080"), false,
+				Collections.singletonList("http://redirecturi:" + PortalUtil.getPortalServerPort(false)), false,
 				Collections.singletonList("everything"), false);
 
 			createOAuth2Application(
