87dd92cae6c798f0f408b125ea933ae9ab2c4c29
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-14 14:06:01 +0200

    LPD-87965 Replace hardcoded 8080 in RedirectEntrySearchTest

--- modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/search/test/RedirectEntrySearchTest.java
+++ modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/search/test/RedirectEntrySearchTest.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.search.test.util.BaseSearchTestCase;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -100,10 +101,13 @@ public class RedirectEntrySearchTest extends BaseSearchTestCase {
 		SearchContext searchContext = SearchContextTestUtil.getSearchContext(
 			group.getGroupId());
 
-		searchContext.setAttribute(
-			"groupBaseURL", "http://localhost:8080/web/guest");
+		String portalURL =
+			"http://localhost:" + PortalUtil.getPortalServerPort(false);
+
 		searchContext.setGroupIds(new long[] {group.getGroupId()});
-		searchContext.setKeywords("http://localhost:8080/web/guest/liferay");
+
+		searchContext.setAttribute("groupBaseURL", portalURL + "/web/guest");
+		searchContext.setKeywords(portalURL + "/web/guest/liferay");
 
 		List<SearchResult> searchResults = _getSearchResults(searchContext);
 
