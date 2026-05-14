854b0da2f9933f4c72cc53fbf3d98682279d287c
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-14 15:45:15 +0200

    LPD-87965 Replace hardcoded 8080 in BaseTestPreparatorBundleActivator

--- modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/BaseTestPreparatorBundleActivator.java
+++ modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/BaseTestPreparatorBundleActivator.java
@@ -26,12 +26,12 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.security.service.access.policy.model.SAPEntry;
 import com.liferay.portal.security.service.access.policy.service.SAPEntryLocalService;
 
@@ -209,7 +209,7 @@ public abstract class BaseTestPreparatorBundleActivator
 		return createOAuth2Application(
 			companyId, user, clientId, "oauthTestApplicationSecret",
 			allowedGrantTypesList,
-			Collections.singletonList("http://redirecturi:8080"),
+			Collections.singletonList("http://redirecturi:" + PortalUtil.getPortalServerPort(false)),
 			rememberDevice, scopeAliasesList, trustedApplication);
 	}
 
@@ -222,7 +222,7 @@ public abstract class BaseTestPreparatorBundleActivator
 		return createOAuth2Application(
 			companyId, user, clientId, "oauthTestApplicationSecret",
 			allowedGrantTypesList,
-			Collections.singletonList("http://redirecturi:8080"),
+			Collections.singletonList("http://redirecturi:" + PortalUtil.getPortalServerPort(false)),
 			scopeAliasesList);
 	}
 
