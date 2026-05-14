20d8a64f5670e8bcc713413d0a4ce3bf8f076d07
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-14 13:56:15 +0200

    LPD-87965 Replace hardcoded 8080 in GetLayoutReportsDataStrutsActionTest

--- modules/apps/layout/layout-reports-test/src/testIntegration/java/com/liferay/layout/reports/web/internal/struts/test/GetLayoutReportsDataStrutsActionTest.java
+++ modules/apps/layout/layout-reports-test/src/testIntegration/java/com/liferay/layout/reports/web/internal/struts/test/GetLayoutReportsDataStrutsActionTest.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -299,7 +300,8 @@ public class GetLayoutReportsDataStrutsActionTest {
 		themeDisplay.setLocale(
 			LocaleUtil.fromLanguageId(_group.getDefaultLanguageId()));
 		themeDisplay.setPlid(layout.getPlid());
-		themeDisplay.setPortalURL("http://localhost:8080");
+		themeDisplay.setPortalURL(
+			"http://localhost:" + PortalUtil.getPortalServerPort(false));
 		themeDisplay.setScopeGroupId(_group.getGroupId());
 		themeDisplay.setSiteGroupId(_group.getGroupId());
 
