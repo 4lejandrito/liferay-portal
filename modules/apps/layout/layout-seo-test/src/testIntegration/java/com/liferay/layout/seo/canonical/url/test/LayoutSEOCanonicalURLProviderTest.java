c5137c3032748585bb64dbcffad202d70f26905c
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-14 14:54:29 +0200

    LPD-87965 Replace hardcoded 8080 in LayoutSEOCanonicalURLProviderTest (assertion URL)

--- modules/apps/layout/layout-seo-test/src/testIntegration/java/com/liferay/layout/seo/canonical/url/test/LayoutSEOCanonicalURLProviderTest.java
+++ modules/apps/layout/layout-seo-test/src/testIntegration/java/com/liferay/layout/seo/canonical/url/test/LayoutSEOCanonicalURLProviderTest.java
@@ -8,6 +8,7 @@ package com.liferay.layout.seo.canonical.url.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.seo.canonical.url.LayoutSEOCanonicalURLProvider;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
@@ -101,7 +102,9 @@ public class LayoutSEOCanonicalURLProviderTest {
 				LocaleUtil.GERMAN);
 
 			Assert.assertEquals(
-				"http://localhost:8080/web" + _group.getFriendlyURL(),
+				StringBundler.concat(
+					"http://localhost:", PortalUtil.getPortalServerPort(false),
+					"/web", _group.getFriendlyURL()),
 				_layoutSEOCanonicalURLProvider.getCanonicalURL(
 					_layout, LocaleUtil.US,
 					_portal.getCanonicalURL(
